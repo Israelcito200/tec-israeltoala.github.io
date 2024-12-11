520e55664d59484c8a05008797dcd827ce74e842
Tu Nombre <tu.email@example.com>
2024-12-11 16:31:05 -0500

    nav2

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+document.getElementById('menu-toggle').addEventListener('click', () => {
+    const links = document.getElementById('navbar-links');
+    links.classList.toggle('active');
+  });
+  
